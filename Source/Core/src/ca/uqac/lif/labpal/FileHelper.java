d4d97079a02f40f80a2f6dcbc379f52266419b5f
Sylvain Hallé <shalle@acm.org>
2017-01-18 18:14:03 -0500

    Fixes to file helper

--- Source/Core/src/ca/uqac/lif/labpal/FileHelper.java
+++ Source/Core/src/ca/uqac/lif/labpal/FileHelper.java
@@ -26,10 +26,17 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.URLDecoder;
 import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 import ca.uqac.lif.jerrydog.InnerFileServer;
 
@@ -368,5 +375,91 @@ public class FileHelper
 		}
 		return listing;
 	}
+	
+	/**
+	 * List all files in a folder whose filename matches a regex pattern
+	 * @param clazz A class reference
+	 * @param directory The source folder
+	 * @param glob The pattern to match
+	 * @return The list of file names
+	 */
+	public static List<String> listAllInternalFiles(Class<?> clazz, String directory, String glob)
+	{
+		List<String> listing = new ArrayList<String>();
+		File f = null;
+		try 
+		{
+			URI url = clazz.getResource(directory).toURI();
+			f = new File(url);
+			if (f != null)
+			{
+				for (String uris : f.list())
+				{
+					if (uris.matches(glob))
+					{
+						listing.add(uris);
+					}
+				}
+			}
+		}
+		catch (URISyntaxException e) 
+		{
+			// Do nothing
+		}
+		return listing;
+	}
+	
+	/**
+	   * List directory contents for a resource folder. Not recursive.
+	   * This is basically a brute-force implementation.
+	   * Works for regular files and also JARs.
+	   * 
+	   * @author Greg Briggs
+	   * @param clazz Any java class that lives in the same place as the resources you want.
+	   * @param path Should end with "/", but not start with one.
+	   * @return Just the name of each member item, not the full paths.
+	   * @throws URISyntaxException 
+	   * @throws IOException 
+	   */
+	  public static String[] getResourceListing(Class<?> clazz, String path) throws URISyntaxException, IOException {
+	      URL dirURL = clazz.getClassLoader().getResource(path);
+	      if (dirURL != null && dirURL.getProtocol().equals("file")) {
+	        /* A file path: easy enough */
+	        return new File(dirURL.toURI()).list();
+	      } 
+
+	      if (dirURL == null) {
+	        /* 
+	         * In case of a jar file, we can't actually find a directory.
+	         * Have to assume the same jar as clazz.
+	         */
+	        String me = clazz.getName().replace(".", "/")+".class";
+	        dirURL = clazz.getClassLoader().getResource(me);
+	      }
+	      
+	      if (dirURL.getProtocol().equals("jar")) {
+	        /* A JAR path */
+	        String jarPath = dirURL.getPath().substring(5, dirURL.getPath().indexOf("!")); //strip out only the JAR file
+	        JarFile jar = new JarFile(URLDecoder.decode(jarPath, "UTF-8"));
+	        Enumeration<JarEntry> entries = jar.entries(); //gives ALL entries in jar
+	        Set<String> result = new HashSet<String>(); //avoid duplicates in case it is a subdirectory
+	        while(entries.hasMoreElements()) {
+	          String name = entries.nextElement().getName();
+	          if (name.startsWith(path)) { //filter according to the path
+	            String entry = name.substring(path.length());
+	            int checkSubdir = entry.indexOf("/");
+	            if (checkSubdir >= 0) {
+	              // if it is a subdirectory, we just return the directory name
+	              entry = entry.substring(0, checkSubdir);
+	            }
+	            result.add(entry);
+	          }
+	        }
+	        jar.close();
+	        return result.toArray(new String[result.size()]);
+	      } 
+	        
+	      throw new UnsupportedOperationException("Cannot list files for URL "+dirURL);
+	  }
 
 }

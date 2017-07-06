21b807fa73813058b870d5912652825afb7dcade
Sylvain Hallé <shalle@acm.org>
2017-07-06 12:28:33 -0400

    Fix to file upload in ZIP; not finished, breaks the build

--- Source/Core/src/ca/uqac/lif/labpal/server/UploadCallback.java
+++ Source/Core/src/ca/uqac/lif/labpal/server/UploadCallback.java
@@ -1,6 +1,6 @@
 /*
-  ParkBench, a versatile benchmark environment
-  Copyright (C) 2015-2016 Sylvain Hallé
+  LabPal, a versatile benchmark environment
+  Copyright (C) 2015-2017 Sylvain Hallé
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -17,12 +17,13 @@
  */
 package ca.uqac.lif.labpal.server;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.regex.Matcher;
@@ -70,42 +71,36 @@ public class UploadCallback extends WebCallback
 	public CallbackResponse process(HttpExchange t)
 	{
 		CallbackResponse cbr = new CallbackResponse(t);
-		Map<String,byte[]> parts = getParts(t);
-		if (parts == null || !parts.containsKey("filename"))
+		List<byte[]> parts = getParts(t);
+		
+		if (parts == null || parts.isEmpty())
 		{ 
-			// Baaaad requestn
+			// Baaaad request
 			doBadRequest(cbr, "No file was uploaded");
 			return cbr;
 		}
-		byte[] part = parts.get("filename");
+		byte[] lab_file_contents = getPartContent(parts.get(0));
+		String filenameClone = getPartName(parts.get(0));
 		//upload file name 
-		String filenameClone = new String (parts.get("filename-clone")).trim();
 		String json = null;
 		//if (DownloadCallback.s_zip)
-		if (filenameClone.indexOf(".zip")>-1)
+		if (filenameClone.endsWith(".zip") || filenameClone.endsWith(".labo"))
 		{
-			ZipInputStream zis = null;
+			// This is a zipped file
 			StringBuilder downloadsUrl = new StringBuilder();
 			downloadsUrl.append(m_lab.getDownloadsUrl()).append("/");
 			downloadsUrl.append(filenameClone);
 			//ByteArrayInputStream bis = new ByteArrayInputStream(part);
 			//System.out.println(FileHelper.readToString(bis));
-			try {
-				zis = new ZipInputStream(new FileInputStream(downloadsUrl.toString()));
-			} catch (FileNotFoundException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			
-			//ZipInputStream zis = new ZipInputStream(bis);
+			ZipInputStream zis = new ZipInputStream(new ByteArrayInputStream(lab_file_contents));
 			ZipEntry entry;
 			byte[] contents = null;
 			try
 			{
-				
+
 				//while((entry = zis.getNextEntry()) != null)
 				entry = zis.getNextEntry();
-				while(entry != null)
+				while (entry != null)
 				{
 					//String name = entry.getName();
 					contents = extractFile(zis);
@@ -115,7 +110,8 @@ public class UploadCallback extends WebCallback
 			catch (IOException e) 
 			{
 				// Baaaad request
-				doBadRequest(cbr, "Could not extract a lab from the file");
+				doBadRequest(cbr, "Could not extract a lab from the file. The message is: <pre>" + e.getMessage() + "</pre>");
+				return cbr;
 			}
 			if (contents != null)
 			{
@@ -124,10 +120,7 @@ public class UploadCallback extends WebCallback
 		}
 		else
 		{
-			if (part != null)
-			{
-				json = new String(part).trim();
-			}
+			json = new String(lab_file_contents).trim();
 		}
 		if (json == null)
 		{
@@ -179,7 +172,7 @@ public class UploadCallback extends WebCallback
 		cbr.setContents(file_contents);
 		return cbr;
 	}
-	
+
 	protected void doBadRequest(CallbackResponse cbr, String message)
 	{
 		cbr.setCode(CallbackResponse.HTTP_BAD_REQUEST);
@@ -212,15 +205,142 @@ public class UploadCallback extends WebCallback
 
 	/**
 	 * Gets the parts of a multipart message.
+	 * @param t The exchange
+	 * @return The parts
+	 */
+	protected static List<byte[]> getParts(HttpExchange t)
+	{
+		InputStream is = t.getRequestBody();
+		List<byte[]> byte_parts = null;
+		try
+		{
+			byte[] byte_contents = streamToBytes(is);
+			// Split the array of bytes based on the delimiter
+			byte_parts = splitArray(byte_contents);
+		}
+		catch (IOException e)
+		{
+			// TODO: something better than that
+			e.printStackTrace();
+		}
+		return byte_parts;
+	}
+	
+	/**
+	 * Gets the array of bytes corresponding to the contents of a part in a
+	 * multi-part request
+	 * @param part The part
+	 * @return The byte array with the contents of that part
+	 */
+	protected static byte[] getPartContent(byte[] part)
+	{
+		int num_cr = 0;
+		for (int i = 0; i < part.length; i++)
+		{
+			if (part[i] == 13)
+			{
+				num_cr++;
+			}
+			else
+			{
+				num_cr = 0;
+			}
+			if (num_cr == 2)
+			{
+				byte[] content = new byte[part.length - i - 1];
+				for (int j = i + 1; j < part.length; j++)
+				{
+					content[j - i - 1] = part[j];
+				}
+				return content;
+			}
+		}
+		return null;
+	}
+	
+	protected static List<byte[]> splitArray(byte[] array)
+	{
+		List<byte[]> parts = new LinkedList<byte[]>();
+		byte[] boundary = null;
+		int last = 0;
+		for (int i = 0; i < array.length && (boundary == null || i < array.length - boundary.length); i++)
+		{
+			if (boundary == null)
+			{
+				if (array[i] != 13)
+					continue;
+				else
+				{
+					boundary = new byte[i - 1];
+					for (int j = 0; j < i - 1; j++)
+					{
+						boundary[j] = array[j];
+					}
+					last = i;
+					continue;
+				}
+			}
+			if (isDelimiter(array, boundary, i))
+			{
+				// A delimiter starts here
+				if (i - last > 0)
+				{
+					byte[] new_part = new byte[i - last];
+					for (int j = last; j < i; j++)
+					{
+						new_part[j - last] = array[j];
+					}
+					parts.add(new_part);
+				}
+				// Skip forward by the length of the delimiter
+				i += boundary.length - 1; //-1 since the loop will increment i
+			}
+		}
+		return parts;
+	}
+	
+	protected static boolean isDelimiter(byte[] array, byte[] boundary, int position)
+	{
+		for (int i = 0; i < boundary.length; i++)
+		{
+			if (array[position + i] != boundary[i])
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * Converts an input stream into a byte array
+	 * @param is The input stream
+	 * @return The byte array
+	 * @throws IOException
+	 */
+	protected static byte[] streamToBytes(InputStream is) throws IOException
+	{
+		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+		int nRead;
+		byte[] data = new byte[16384];
+		while ((nRead = is.read(data, 0, data.length)) != -1) 
+		{
+			buffer.write(data, 0, nRead);
+		}
+		buffer.flush();
+		return buffer.toByteArray();
+	}
+
+	/**
+	 * Gets the parts of a multipart message.
 	 * <strong>Caveat emptor:</strong> this method works correctly with
 	 * text, but not with binary files.
 	 * @param t The exchange
 	 * @return The parts
 	 */
-	protected static Map<String,byte[]> getParts(HttpExchange t)
+	protected static Map<String,byte[]> getPartsOld(HttpExchange t)
 	{
 		InputStream is = t.getRequestBody();
-		Scanner sc = new Scanner(is);
+		Scanner sc = new Scanner(is, "ASCII");
 		Map<String,byte[]> out = new HashMap<String,byte[]>();
 		ByteArrayOutputStream bos = new ByteArrayOutputStream();
 		String filename = "";
@@ -257,7 +377,8 @@ public class UploadCallback extends WebCallback
 					filename = "";
 					continue;
 				}
-				bos.write(line.getBytes());
+				byte[] line_bytes = line.getBytes("ASCII");
+				bos.write(line_bytes);
 				bos.write((char) 13); // The scanner trimmed this byte when reading
 			}
 		}
@@ -267,6 +388,22 @@ public class UploadCallback extends WebCallback
 		}
 		sc.close();
 		return out;
+	}
+
+	/**
+	 * Trims an array of bytes from its first and last byte. This is used to
+	 * remove carriage returns from a field in an HTTP request
+	 * @param in_array The array of bytes
+	 * @return The new array of bytes without the first and last bytes
+	 */
+	protected static byte[] trimByteArray(byte[] in_array)
+	{
+		byte[] file_contents = new byte[in_array.length - 2];
+		for (int i = 1; i < in_array.length - 1; i++)
+		{
+			file_contents[i - 1] = in_array[i];
+		}
+		return file_contents;
 	}
 
 }

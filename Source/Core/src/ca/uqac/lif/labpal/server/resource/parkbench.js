58384f217326929e08494087e2c1e545d566898f
Sylvain Hallé <shalle@acm.org>
2017-07-06 10:22:29 -0400

    Modified filter JS code. The original prevented typing invalid characters by checking on each keypress if the character was valid, and removing it immediately from the textbox. This is a slightly odd way of enforcing valid input (and it doesn't work if you copy-paste a string with multiple invalid symbols at the end, as only the last one will be deleted).

--- Source/Core/src/ca/uqac/lif/labpal/server/resource/parkbench.js
+++ Source/Core/src/ca/uqac/lif/labpal/server/resource/parkbench.js
@@ -3,47 +3,9 @@ $(document).ready(function() {
     $("table.exp-table .top-checkbox").click(function() {
       $(this).closest("table.exp-table").find(".side-checkbox").not(this).prop("checked", this.checked);
     });
-    
-    //authorized inputs
-   $("table.exp-table .top-filter").keyup(function() {
-    var input = $(this).val();
-    var regex = new RegExp("^[0-9|,|-]+$");
-    if (!regex.test(input)) $(this).val(input.substr(0, input.length-1));
-   });
    
    //filter by enumeration or/and interval
-   $("table.exp-table .btn-filter").click(function() {
-    //var hideLigne = $("table.exp-table .top-filter").val().trim();
-    var hideLigne = $("table.exp-table .top-filter").val();
-    //
-    if (hideLigne ==""){
-     $("table.exp-table .tr").show();
-     $("table.exp-table .side-checkbox").prop("disabled", false);
-    }
-    //
-    else {
-	    $("table.exp-table .tr").hide();
-	    $("table.exp-table .side-checkbox").prop("disabled", true);
-	    var hideLigneArray = hideLigne.split(",");
-		    for(var i= 0; i < hideLigneArray.length; i++){
-		    if (hideLigneArray[i]!='') {//inputs like ',9,'
-		     if(hideLigneArray[i].indexOf('-')>-1){
-		     var hideLigneArray2 = hideLigneArray[i].split("-");
-		     if (hideLigneArray2[0]!='' && hideLigneArray2[1]!='') {//inputs like '-5'
-		      for(var k= parseInt(hideLigneArray2[0]); k <= parseInt(hideLigneArray2[1]); k++){	      
-		    	$("table.exp-table .tr-"+k).show();
-		    	$("table.exp-table .side-checkbox-"+k).prop("disabled", false);
-		      }
-		     }//if (hideLigneArray2[0]!='' && hideLigneArray2[1]!='')  
-		     }
-		     else {
-		     $("table.exp-table .tr-"+hideLigneArray[i]).show();
-		     $("table.exp-table .side-checkbox-"+hideLigneArray[i]).prop("disabled", false);
-		     }
-		     }//if (hideLigneArray[i]!='')
-		    }
-	    }
-    });
+   $("#experiment-filter .btn-filter").click(filter_experiments); 
 
     $("table.exp-table").tablesorter();
     $(".pulldown-contents").hide();
@@ -116,11 +78,47 @@ $(document).ready(function() {
   $(" .btn-download").click(function(event) {
        event.preventDefault();
        afficherPopupConfirmationLien('Select the type of file to download ');
-     });
-
-    
-    
-    
-    
-    
+     });  
 });
+
+/**
+ * Filters the list of experiments in a page based on a text
+ * string specifiying range
+ * @returns Nothing
+ */
+function filter_experiments() 
+{
+    //var hideLigne = $("table.exp-table .top-filter").val().trim();
+    var hideLigne = $(".top-filter").val().trim();
+    //
+    if (hideLigne ==""){
+     $("table.exp-table .tr").show();
+     $("table.exp-table .side-checkbox").prop("disabled", false);
+    }
+    //
+    else {
+	    $("table.exp-table .tr").hide();
+	    $("table.exp-table .side-checkbox").prop("disabled", true);
+	    hideLigne = hideLigne.replace(/[^0-9,\-]/g, "");
+	    console.log(hideLigne);
+	    var hideLigneArray = hideLigne.split(",");
+	    
+		    for(var i= 0; i < hideLigneArray.length; i++){
+		    if (hideLigneArray[i]!='') {//inputs like ',9,'
+		     if(hideLigneArray[i].indexOf('-')>-1){
+		     var hideLigneArray2 = hideLigneArray[i].split("-");
+		     if (hideLigneArray2[0]!='' && hideLigneArray2[1]!='') {//inputs like '-5'
+		      for(var k= parseInt(hideLigneArray2[0]); k <= parseInt(hideLigneArray2[1]); k++){	      
+		    	$("table.exp-table .tr-"+k).show();
+		    	$("table.exp-table .side-checkbox-"+k).prop("disabled", false);
+		      }
+		     }//if (hideLigneArray2[0]!='' && hideLigneArray2[1]!='')  
+		     }
+		     else {
+		     $("table.exp-table .tr-"+hideLigneArray[i]).show();
+		     $("table.exp-table .side-checkbox-"+hideLigneArray[i]).prop("disabled", false);
+		     }
+		     }//if (hideLigneArray[i]!='')
+		    }
+	    }
+};

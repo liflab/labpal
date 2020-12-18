1abe74fcf2064ee91cd29bff3c9d7c9b57ff22aa
Sylvain Hallé <shalle@acm.org>
2020-12-18 12:41:03 -0500

    Incremented version number

--- Source/Core/src/ca/uqac/lif/labpal/Laboratory.java
+++ Source/Core/src/ca/uqac/lif/labpal/Laboratory.java
@@ -98,7 +98,7 @@ public abstract class Laboratory implements OwnershipManager
   /**
    * The revision version number
    */
-  private static final transient int s_revisionVersionNumber = 7;
+  private static final transient int s_revisionVersionNumber = 8;
 
   /**
    * The set of experiments this lab has access to
